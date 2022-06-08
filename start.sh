13862a85a19efac7da8e3e832cab66f6b6f1c003
AK <ksavinash49@gmail.com>
2022-06-08 21:50:13 +0530

    load env

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -e
 echo "============================================================="
 echo "Starting db migrations --->>>>"
 echo
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 echo
 echo "Done db migrations --->>>>"
